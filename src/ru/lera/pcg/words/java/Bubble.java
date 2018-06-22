5870a8b22de202a1a74d2d67251c29a5f877a331
LeraValera93 <39859975+LeraValera93@users.noreply.github.com>
2018-06-22 22:34:31 +0500

    Сортировка строки пузырьком Добавление двух функций сортировки по возрастанию и убыванию символов строки

--- src/ru/lera/pcg/words/java/Bubble.java
+++ src/ru/lera/pcg/words/java/Bubble.java
@@ -1,34 +1,56 @@
 package ru.lera.pcg.words.java;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bubble {
 
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Введите любое слово: ");
 
     String word = sc.nextLine();
-
-    char[] ch;
-    ch = word.toCharArray();
-
     System.out.println("Содержимое строки до сортировки: " + word);
 
-    for (int i = ch.length - 1; i > 0; i--) {
+    System.out.println(function_increase(word));
+    System.out.println(function1_decrease(word));
+}
+
+
+  private static String function_increase(String word) {
+    char[] ch1;
+    ch1 = word.toCharArray();
+
+    for (int i = ch1.length - 1; i > 0; i--) {
       for (int j = 0; j < i; j++) {
 
-        if (ch[j] > ch[j + 1]) {
-          int c = ch[j];
-          ch[j] = ch[j + 1];
-          ch[j + 1] = (char) c;
+        if (ch1[j] > ch1[j + 1]) {
+          int c = ch1[j];
+          ch1[j] = ch1[j + 1];
+          ch1[j + 1] = (char) c;
+        }
+      }
+    }
+    String word1 = new String(ch1);
+    return word1;
+  }
+
+  private static String function1_decrease(String word) {
+    char[] ch2;
+    ch2 = word.toCharArray();
+    for (int y = ch2.length - 1; y > 0; y--) {
+      for (int z = 0; z < y; z++) {
+
+        if (ch2[z] < ch2[z + 1]) {
+          int c = ch2[z];
+          ch2[z] = ch2[z + 1];
+          ch2[z + 1] = (char) c;
 
         }
       }
     }
-    System.out.print("Содержимое строки после сортировки: ");
-    System.out.println(ch);
+    String word2 = new String(ch2);
+    return word2;
   }
 }
+
 
